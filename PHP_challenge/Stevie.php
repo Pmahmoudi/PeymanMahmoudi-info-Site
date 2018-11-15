5c8d9802c126ced1e019470daf73e35243411f65
inleidingJava <peyman_mah6@yahoo.com>
2018-11-15 20:09:59 +0100

    first comit

---
+++ PHP_challenge/Stevie.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Album example for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+<header>
+
+    <div class="navbar navbar-dark bg-dark box-shadow">
+        <div class="container d-flex justify-content-between">
+            <a href="index.php" class="navbar-brand d-flex align-items-center">
+                <strong>SpotiTube</strong>
+            </a>
+            <a href="index.php" class="navbar-brand d-flex align-items-center">
+                <strong>Home</strong>
+            </a>
+            <a href="playlists.php" class="navbar-brand d-flex align-items-center">
+                <strong>Playlists</strong>
+            </a>
+
+        </div>
+    </div>
+    <main role="main">
+
+        <section class="jumbotron text-center">
+            <div class="container">
+                <h1 class="jumbotron-heading">SpotiTube</h1>
+                <p class="lead text-muted">This is a template for a simple marketing or informational website. It icludes a large callout called a jumbotron and three supporting pieces of content.
+                    Use it as a starting point to create something more unique.</p>
+            </div>
+        </section>
+    </main>
+</header>
+
+<footer>
+    <hr>
+    <p>@ 2016 Company, lnc.</p>
+</footer>
+</body>
+</html>
---
+++ PHP_challenge/playlists.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Album example for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+<header>
+    <header>
+
+        <div class="navbar navbar-dark bg-dark box-shadow">
+            <div class="container d-flex justify-content-between">
+                <a href="index.php" class="navbar-brand d-flex align-items-center">
+                    <strong>SpotiTube</strong>
+                </a>
+                <a href="index.php" class="navbar-brand d-flex align-items-center">
+                    <strong>Home</strong>
+                </a>
+                <a href="playlists.php" class="navbar-brand d-flex align-items-center">
+                    <strong>Playlists</strong>
+                </a>
+
+            </div>
+        </div>
+    <main role="main">
+
+        <section class="jumbotron text-center">
+            <div class="container">
+                <h1 class="jumbotron-heading">SpotiTube</h1>
+                <p class="lead text-muted">This is a template for a simple marketing or informational website. It icludes a large callout called a jumbotron and three supporting pieces of content.
+                    Use it as a starting point to create something more unique.</p>
+            </div>
+        </section>
+
+            <div class="container d-flex justify-content-between">
+
+        <h1>
+            PlayList
+        </h1>
+                <a href="Greg.php"class="ac-gf-directory-column-section-link">  Greg Koch Band </a>
+                 <a href="Robben.php"> Robben Ford </a>
+                 <a href="Stevie.php" > Stevie Ray Vaughan </a>
+            </div>
+        </div>
+    </main>
+</header>
+
+<footer>
+    <hr>
+<p>@ 2016 Company, lnc.</p>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
